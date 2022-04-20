11f3cb4b79e0f0b493a7faa56415b4d643577c26
Alex Noble-James <alexnoble143.an@gmail.com>
2022-04-19 23:29:20 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = express.Router();
+// const router = express.Router();
 const app = express();
 
 const PORT = process.env.PORT || 3001;
